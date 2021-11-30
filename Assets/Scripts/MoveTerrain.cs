4f8371618a6fcfa960665d62965b4181773be2a2
TimBlanchard <timothee.blanchard37@gmail.com>
2021-12-01 00:11:59 +0100

    Reset old repo content

---
+++ Assets/Scripts/MoveTerrain.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTerrain : MonoBehaviour
+{
+    public Vector3 Direction;
+    // public float Speed;
+
+    private Global GlobalRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GlobalRef = GameObject.FindObjectOfType<Global>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(GlobalRef.Speed * Direction * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Spawner/SpawnerByTriggerLeave.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerByTriggerLeave : MonoBehaviour
+{
+    public GameObject[] ObjPrefabs;
+
+    public int Min = -1;
+    public int Max = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Sisi");
+        SpawnPrefab();
+    }
+
+    public void SpawnPrefab() {
+        int index = Random.Range(0, ObjPrefabs.Length);
+
+        GameObject spawnee = GameObject.Instantiate(ObjPrefabs[index]);
+
+        Vector3 spawnerPos = transform.position;
+
+        spawnerPos.x += Random.Range(Min, Max);
+
+        spawnee.transform.position = spawnerPos;
+
+    }
+}

993a19851b7fbaab88baf5b7edcd948cc660b012
TimBlanchard <timothee.blanchard37@gmail.com>
2021-12-01 23:10:18 +0100

    HorseRhythmRace - Add miss and hit notes detection

---
+++ Assets/Scripts/RhythmGame/GameManager.cs
@@ -0,0 +1,44 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource _audioSource;
+    public bool startPlaying;
+    public BeatScroller _beatScroller;
+
+    public static GameManager instance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startPlaying)
+        {
+            if (Input.anyKeyDown)
+            {
+                startPlaying = true;
+                _beatScroller.hasStarted = true;
+
+                _audioSource.Play();
+            }
+            
+        }
+    }
+
+    public void NoteHit()
+    {
+        Debug.Log("Hit On Time");
+    }
+
+    public void NoteMissed()
+    {
+        Debug.Log("Missed");
+    }
+}
